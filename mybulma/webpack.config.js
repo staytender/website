02180998da95cdf1ad05505aff790a4ad56570e5
Marcel Jendro <Marcel.Jendro@arvato-scs.com>
2024-08-22 16:19:36 +0200

    Working compiler

---
+++ mybulma/webpack.config.js
@@ -0,0 +1,31 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader"
+			/*{
+               loader: MiniCssExtractPlugin.loader
+            },
+            {
+               // MiniCssExtractPlugin.loader, "css-loader"
+			   loader: "sass-loader"
+            }*/
+        ],
+      },
+    ],
+  },
+};
